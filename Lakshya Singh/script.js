5dfec140727c9de048be4d987d60ee61e6c0091a
king-11 <lakshay.singh1108@gmail.com>
2019-12-15 22:00:39 +0530

    Game End dealt with

--- Lakshya Singh/script.js
+++ Lakshya Singh/script.js
@@ -1,18 +1,19 @@
 function getResult(board){
-
+	let store;
 	for(let i = 0; i < 3; i++){
 		let sum = 0;
 		for(let j = 0; j < 3; j++){
 			sum += (+board[i][j]);
 		}
 		if(sum == 3){
-			alert("X wins");
+			store = "X";
 			reset(board);
-			return false;
+			return store;
 		}
 		else if(sum == -3){
-			alert("O wins");
-			return false;
+			store = "O";
+			reset(board);
+			return store;
 		}
 	}
 
@@ -22,14 +23,14 @@ function getResult(board){
 			sum += (+board[j][i]);
 		}
 		if(sum == 3){
-			alert("X wins");
+			store = "X";
 			reset(board);
-			return false;
+			return store;
 		}
 		else if(sum == -3){
-			alert("O wins");
+			store = "O";
 			reset(board);
-			return false;
+			return store;
 		}
 	}
 
@@ -37,17 +38,15 @@ function getResult(board){
 	var dig2 = (+board[0][2]) + (+board[1][1]) + (+board[2][0]);
 
 	if(dig1 == 3 || dig2 == 3){
-			alert("X wins");
+			store = "X";
 			reset(board);
-			return false;
+			return store;
 		}
 	else if(dig1 == -3 || dig2 == -3){
-		alert("O wins");
+		store = "O";
 		reset(board);
-		return false;
+		return store;
 	}
-
-	return true;
 }
 
 
@@ -77,9 +76,14 @@ function start(element,count){
 }
 
 function add(element){
+	
 	if(counter == -1){
 		alert("User 1 select X or O")
 		return;
+	}
+
+	if(getResult(board)!= undefined){
+		$('button[id^="item"]').prop('disabled', true);
 	}
 
 	let store = element.getAttribute("id");
@@ -98,7 +102,10 @@ function add(element){
 
 	counter++;
 	if(counter > 4){
-		getResult(board);
+		if(getResult(board)!= undefined){
+			winner = getResult(board);
+			alert(`${winner} wins`)
+		}
 	}
+	
 }
-
