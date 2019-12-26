94677d383b1614965d8ed7104523c47ad35b0e15
Lakshya Singh <lakshay.singh1108@gmail.com>
2019-12-26 15:36:24 +0530

    Submission: king-11 (#19)
    * first commit
    
    * Game End dealt with
    
    * Background stretched
    
    * for deploying
    
    * other method
    
    * name change
    
    * Set theme jekyll-theme-cayman
    
    * Revert "Set theme jekyll-theme-cayman"
    
    This reverts commit 92a19f99efac7660de3e21a0a8b43d7a478bb95e.
    
    * Dual Player Mode Ready
    
    * deleted others
    
    * Styling Dual Player Done and added score function
    
    * Made a bit responsive
    
    * Just one thing left
    
    * The Endgame Now
    
    * Name Change
    
    * pulled from orginal repo

---
+++ king-11/scripts/script1.js
@@ -0,0 +1,248 @@
+function gameState(board) {
+  let store;
+  for (let i = 0; i < 3; i++) {
+    let sum = 0;
+    for (let j = 0; j < 3; j++) {
+      sum += (+board[i][j]);
+    }
+    if (sum == 3) {
+      store = "X";
+      return store;
+    } else if (sum == -3) {
+      store = "O";
+      return store;
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    let sum = 0;
+    for (let j = 0; j < 3; j++) {
+      sum += (+board[j][i]);
+    }
+    if (sum == 3) {
+      store = "X";
+      return store;
+    } else if (sum == -3) {
+      store = "O";
+      return store;
+    }
+  }
+
+  var dig1 = (+board[0][0]) + (+board[1][1]) + (+board[2][2]);
+  var dig2 = (+board[0][2]) + (+board[1][1]) + (+board[2][0]);
+
+  if (dig1 == 3 || dig2 == 3) {
+    store = "X";
+    return store;
+  } else if (dig1 == -3 || dig2 == -3) {
+    store = "O";
+    return store;
+  }
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (+board[i][j] == 0) {
+        return false;
+      }
+    }
+  }
+  return store;
+
+}
+
+function getResult(user, board) {
+  result = gameState(board);
+  if (result === undefined) {
+    return "Tie";
+  }
+  if (result === user) {
+    return true;
+  }
+  if (result === false) {
+    return false;
+  }
+}
+
+
+function reset(array) {
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      array[i][j] = 0;
+    }
+  }
+}
+
+function start(element) {
+  player = element.innerHTML;
+  player = player[5];
+
+  player == "X" ? playerAI = "O" : playerAI = "X";
+  x.disabled = true;
+  o.disabled = true;
+
+  if (!confirm('Do you want to go First')) {
+    document.getElementById(4).disabled = true;
+    document.getElementById(4).innerHTML = playerAI;
+
+    if (playerAI == "X") {
+      realBoard[1][1] = 1;
+    } else {
+      realBoard[1][1] = -1;
+    }
+  }
+}
+
+function resetBoard() {
+  document.querySelectorAll('.input').forEach(elem => {
+    elem.innerHTML = " ";
+    elem.disabled = false;
+  });
+  x.disabled = false;
+  o.disabled = false;
+
+  document.getElementById("Score").style.display = "none";
+  document.getElementById("Score").innerHTML = " ";
+  reset(realBoard)
+  player = playerAI = undefined;
+}
+
+let realBoard = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+]
+
+const x = document.getElementById("X");
+const o = document.getElementById("O");
+
+let player, playerAI;
+
+function addElement(number, user) {
+  let row = Math.floor(number / 3);
+  let col = number % 3;
+
+  if (user == "X") {
+    realBoard[row][col] = 1;
+  } else {
+    realBoard[row][col] = -1;
+  }
+
+  document.getElementById(number).disabled = true;
+
+}
+
+function userPlay(element) {
+  if (player === undefined) {
+    window.navigator.vibrate(200);
+    alert("Please select X or O");
+    return;
+  }
+  addElement(+element.getAttribute("id"), player);
+  element.innerHTML = player;
+
+  if (getResult(player, realBoard) == false) {
+    index = optimalIndex()
+    document.getElementById(index).innerHTML = playerAI;
+    addElement(index, playerAI);
+
+    if (getResult(playerAI, realBoard) == true) {
+      document.getElementById("Score").innerHTML = "Aww!Earth Lost Her Best Defender!"
+      document.getElementById("Score").style.display = "block";
+      window.navigator.vibrate(200);
+      document.querySelectorAll('.input').forEach(elem => {
+        elem.disabled = true;
+      });
+    } else if (getResult(playerAI, realBoard) == "Tie") {
+      document.getElementById("Score").innerHTML = "I see this as an Absolute Win!"
+      document.getElementById("Score").style.display = "block";
+      window.navigator.vibrate(200);
+    }
+  } else {
+    document.getElementById("Score").innerHTML = "I see this as an Absolute Win!"
+    document.getElementById("Score").style.display = "block";
+    window.navigator.vibrate(200);
+  }
+
+}
+
+
+function optimalIndex() {
+  return minimax(realBoard, playerAI, 0);
+}
+
+function minimax(newBoard, user, depth) {
+
+  if (getResult(player, newBoard) == "Tie") {
+    return 0;
+  }
+  if (getResult(player, newBoard)) {
+    return depth - 10;
+  } else if (getResult(playerAI, newBoard)) {
+    return 10 - depth;
+  } else {
+    let cells = [];
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (newBoard[i][j] != 0) continue;
+
+        copyNewBoard = newBoard.map(value => value.map(number => number));
+
+        if (user == "X")
+          copyNewBoard[i][j] = 1;
+        else
+          copyNewBoard[i][j] = -1;
+
+        let vase
+        if (user == playerAI) {
+          vase = minimax(copyNewBoard, player, depth + 1);
+        } else {
+          vase = minimax(copyNewBoard, playerAI, depth + 1)
+        }
+
+        cells.push({
+          sum: vase,
+          cell: {
+            row: i,
+            col: j
+          }
+        });
+      }
+    }
+
+    if (user == playerAI) {
+      const max = _.maxBy(cells, (c) => {
+        return c.sum;
+      });
+      if (depth == 0) {
+        return ((+max.cell.row) * 3 + (+max.cell.col));
+      } else
+        return max.sum;
+    }
+
+    if (user == player) {
+      const min = _.minBy(cells, (c) => {
+        return c.sum;
+      });
+      if (depth == 0) {
+        return ((+min.cell.row) * 3 + (+min.cell.col));
+      } else
+        return min.sum;
+    }
+
+  }
+}
+
+function empty(board) {
+  let count = 0;
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (board[i][j] == 0) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
---
+++ king-11/scripts/script2.js
@@ -0,0 +1,157 @@
+function getResult(board){
+	let store;
+	for(let i = 0; i < 3; i++){
+		let sum = 0;
+		for(let j = 0; j < 3; j++){
+			sum += (+board[i][j]);
+		}
+		if(sum == 3){
+			store = "X";
+			return store;
+		}
+		else if(sum == -3){
+			store = "O";
+			return store;
+		}
+	}
+
+	for(let i = 0; i < 3; i++){
+		let sum = 0;
+		for(let j = 0; j < 3; j++){
+			sum += (+board[j][i]);
+		}
+		if(sum == 3){
+			store = "X";
+			return store;
+		}
+		else if(sum == -3){
+			store = "O";
+			return store;
+		}
+	}
+
+	var dig1 = (+board[0][0]) + (+board[1][1]) + (+board[2][2]);
+	var dig2 = (+board[0][2]) + (+board[1][1]) + (+board[2][0]);
+
+	if(dig1 == 3 || dig2 == 3){
+			store = "X";
+			return store;
+		}
+	else if(dig1 == -3 || dig2 == -3){
+		store = "O";
+		return store;
+	}
+}
+
+
+function reset(array){
+	for(var i = 0; i < 3; i++){
+		for(var j = 0; j < 3; j++){
+			array[i][j] = 0;
+		}
+	}
+}
+
+let board = [
+		[0,0,0],
+		[0,0,0],
+		[0,0,0],
+	]
+
+const x = document.getElementById("X");
+const o = document.getElementById("O");
+
+let counter = -1, max;
+let num1 = 0;
+let num2 = 0;
+
+let Play1, Play2;
+
+let player1 = prompt('Player1 enter your Name please','Player1');
+let player2 = prompt('Player2 enter your Name please','Player2');
+
+while(player1 == null || player2 == null){
+	window.navigator.vibrate(100);
+	player1 = prompt('Player1 enter your Name please','Player1');
+	player2 = prompt('Player2 enter your Name please','Player2');
+}
+
+function start(element,count){
+	Play1 = element.innerHTML;
+	Play1 = Play1[5];
+
+	Play1 == "X" ? Play2 = "O" : Play2 = "X";
+
+	counter = count;
+	x.disabled = true;
+	o.disabled = true;
+	max = count + 9;
+}
+
+function add_dp(element){
+
+	if(counter == -1){
+		window.navigator.vibrate(200);
+		alert(`${player1} select X or O`)
+		return;
+	}
+
+	let store = element.getAttribute("id");
+	element.disabled = true;
+
+	row = Math.floor(+store[4]/3);
+	col = +store[4]%3;
+	if(counter&1){
+		board[row][col] = 1;
+		element.innerHTML = 'X';
+	}
+	else{
+		board[row][col] = -1;
+		element.innerHTML = 'O';
+	}
+
+	counter++;
+
+	if(counter > 4){
+		if(getResult(board)!= undefined){
+			getResult(board) == Play1 ? num1++ : num2++;
+
+			window.navigator.vibrate(200);
+			document.getElementById("Score").style.display = "block";
+			document.getElementById("Score").innerHTML = (getResult(board) == Play1 ? `${player1} wins`: `${player2} wins`);
+			document.querySelectorAll('.input').forEach(elem => {
+  			elem.disabled = true;
+			});
+		}
+	}
+
+	if(counter == max){
+		document.getElementById("Score").style.display = "block";
+		document.getElementById("Score").innerHTML = 'I see this as an Absolute Win!';
+	}
+
+}
+
+function resetBoardDp(){
+	document.querySelectorAll('.input').forEach(elem => {
+		elem.innerHTML = " ";
+		elem.disabled = false;
+	});
+	x.disabled = false;
+	o.disabled = false;
+	counter = -1;
+
+	document.getElementById("Score").style.display = "none";
+	document.getElementById("Score").innerHTML = " ";
+	reset(board)
+}
+
+function getScoreDp(){
+	document.getElementById("Score").style.display = "block";
+	document.getElementById("Score").innerHTML = `${player1} : ${num1}<br><br>${player2} : ${num2}`;
+}
+
+function resetScore(){
+	num1 = 0;
+	num2 = 0;
+}
